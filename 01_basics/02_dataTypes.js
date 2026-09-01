3099da22c2b2ca8c8c4f39217961564cd7168153
Nandini Sharma <06sharmanandini@gmail.com>
2026-09-01 16:58:32 +0000

    saved basics files

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+let name = "jayesh"
+let age = 18
+let state;
+
+// PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not yet assigned 
+// symbol => unique 
+
+// object
+
+console.log(typeof "jignesh");
+console.log(typeof "23");
+console.log(typeof 56);
+console.log(typeof true);
+console.log(typeof null); //returns object
+console.log(typeof undefined); //returns undefined
+
+// H.W read specification from ECMA and mainly from mdc
