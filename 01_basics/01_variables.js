ceef9fae969349c4163991dd8de39bcf7930157c
Nandini Sharma <06sharmanandini@gmail.com>
2026-09-01 17:24:43 +0000

    saved basics files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+//console.log("Krishna")
+
+// const cannot be changed inshort you locked the value
+const accountId = 12345
+let accountEmail = "krishn@google.com"
+var accountPassword = "12768"
+accountCity = "Shilong"
+
+//accountId = 2 // not allowed
+accountEmail = "tvtv@g.com"
+accountPassword = "989898"
+accountCity = "Manipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
