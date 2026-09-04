4bffecc0a59cfe86206905ce14f983d5c21afa7c
Nandini Sharma <06sharmanandini@gmail.com>
2026-09-04 17:47:07 +0000

    your commit message here

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "vani"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1; false => 0
+// null => 0
+// undefined => undefined
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// 2 => true
+// "" => false
+// "mika" => true 
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
