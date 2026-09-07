eeb84fe766aa9b74a9660f6f5afb8b4ebd27b812
Nandini Sharma <06sharmanandini@gmail.com>
2026-09-07 14:29:08 +0000

    msg

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,4 +26,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 44
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+/***************************** Operations ************************ */
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 4);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " mahesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + 2); // return 3
+// console.log("1" + "2"); // return 12
+// console.log("1" + 2); // return 12
+// console.log(1 + "2"); // return 12
+// console.log("1" + 2 + 2); // return 122 (bcoz of string)
+// console.log(1 + 2 + "2"); // return 32 (ECMA script guideline inside ToPrimitive)
+
+// console.log( 3+ 4 * 5 % 3); // bad code writing use more parenthesis
+
+/***************************Tricy (special) conversion **************************************/
+// console.log(+true); // cause true is boolean value and it is 1 so if it has increment then it will return value 1
+// cosnole.log(true+); // shows syntax error
+// console.log(+""); // return 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https;//tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
