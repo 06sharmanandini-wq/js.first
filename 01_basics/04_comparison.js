a2ab70254b03cd8cf9096e0f3e4e399447b149d2
Nandini Sharma <06sharmanandini@gmail.com>
2026-09-09 12:57:55 +0000

    age section need to be checked.

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(2 > "1");
+
+// console.log(null > 0);
+// console.log(null === 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+                                       // these kind of comparison is confusing cause output is always different as sometimes it converse sometimes not
+// console.log(undefined === 0);
+// console.log(undefined >= 0);
+// console.log(undefined > 0);
+
+// === not only check value but also check data type as well
+// == check but do comversion of data 
+
+console.log("2" == 2); // return true bcoz == will conversion
+console.log("2" === 2); // return false bcoz === will not only compare value but also compare datatypes
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoogedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 3245678987655413256700987654n
+
+// Reference (Non Primitive)
+// Array, Objects, Functions.
+
+const heros = ["Shaktiman", "Spiderman", "Ironman", "Thor", "Loki"]  // Array
+{
+    name: "Dignesh",
+    age: 24,
+
+
+}
